dc311944a243ab5fdd87ddded61a1bff297f8688
kryptopacy <kryptopacy@gmail.com>
2026-07-06 07:21:38 +0100

    feat: update reactCompiler in next.config.ts experimental settings

--- next.config.ts
+++ next.config.ts
@@ -79,6 +79,7 @@ const withPWA = withPWAInit({
 
 const nextConfig: NextConfig = {
   experimental: {
+    // @ts-ignore - Next.js types may not be fully up to date with canary compiler flags
     reactCompiler: true,
   },
   allowedDevOrigins: ['127.0.0.1'],
